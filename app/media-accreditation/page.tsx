20c2978291cf5f029d2cede7be719ceb871634f6
foliceasummit <foliceasummit@gmail.com>
2026-01-12 21:10:29 +0200

    Fix missing Footer import in media accreditation page

--- app/media-accreditation/page.tsx
+++ app/media-accreditation/page.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from 'framer-motion'
 import Navigation from '../components/layout/Navigation'
+import Footer from '../components/layout/Footer'
 
 export default function MediaAccreditationPage() {
   return (
